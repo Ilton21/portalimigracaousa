9a4c4abba70207d928dfedcb737c605667ea3aea
Ilton Oliveira <Ilton@MacBook-Pro-de-Debora.local>
2025-09-28 22:31:31 -0400

    fix: substituindo <a> por <Link> no Header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,12 @@
-export default function Header() {
   return (
     <header style={{ padding: "20px", background: "#f5f5f5" }}>
       <h1>Portal Imigração USA</h1>
       <nav>
         <a href="/" style={{ marginRight: "15px" }}>Início</a>
         <a href="/about">Sobre</a>
+import Link from 'next/link';
+
+<Link href="/about">Sobre</Link>
       </nav>
     </header>
   );
