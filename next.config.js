8866e8fe28b75ba7ca764fa538e21565003fb194
Ilton Oliveira <Ilton@MacBook-Pro-de-Debora.local>
2025-11-09 19:15:37 -0500

    Build otimizado e aviso do workspace resolvido

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  outputFileTracingRoot: __dirname, // define o root corretamente
+};
+
+module.exports = nextConfig;
+
